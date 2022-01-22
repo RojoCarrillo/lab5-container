2f0be399332212179c252dea99b2d0a11f24276f
Juanjo Rojo <juanjorojocarrillo55@gmail.com>
2022-01-22 18:45:19 +0100

    añadiendo el output

---
+++ output_manejando_container.sh
@@ -0,0 +1,40 @@
+Version del sistema operativo
+PRETTY_NAME="Debian GNU/Linux 10 (buster)"
+NAME="Debian GNU/Linux"
+VERSION_ID="10"
+VERSION="10 (buster)"
+VERSION_CODENAME=buster
+ID=debian
+HOME_URL="https://www.debian.org/"
+SUPPORT_URL="https://www.debian.org/support"
+BUG_REPORT_URL="https://bugs.debian.org/"
+					
+Contenido del directorio que estamos viendo
+README.md
+data
+manejando_container.sh
+output_manejando_container.sh
+singularity
+submit_eval_singularity.sh
+					
+El path del directorio que estamos viendo
+/home/alumno22/lab5-container
+					
+La version de python que hay instalada
+Python 3.9.1
+					
+La version del compilador de C++ que tenemos instalada
+g++ (Debian 8.3.0-6) 8.3.0
+Copyright (C) 2018 Free Software Foundation, Inc.
+This is free software; see the source for copying conditions.  There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+					
+Ejecutando k-mers13 en C++
+Start
+Number of generated k-mers: 67108864
+Finish!
+
+real	0m0,732s
+user	0m0,614s
+sys	0m0,078s
